cafc10a70f452f861c43bf02c6a2b6984fb5de13
csibivili <csibi.vili@gmail.com>
2022-05-27 10:27:31 +0200

    Add material ui

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,37 @@
-import './App.css'
+import { Box, Paper, Typography, CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
 
 function App() {
-  return <div className="app"></div>
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#AB46D2',
+        light: '#d14684',
+        dark: '#4655d1',
+        contrastText: '#55D8C1',
+      },
+      secondary: {
+        main: '#FF6FB5',
+        light: '#d06fff',
+        dark: '#ffa16f',
+        contrastText: '#FCF69C',
+      },
+    },
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <Box height="100vh" display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+        <Paper elevation={3} sx={{ padding: '1rem', backgroundColor: 'primary.dark' }}>
+          <Typography color="secondary.main" variant="h1">
+            Starter App
+          </Typography>
+        </Paper>
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 export default App
